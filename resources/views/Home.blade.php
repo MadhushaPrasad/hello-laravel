e4342eb890dcd6b0581485d3545d312a91fc92a6
MadhushaPrasad <madushaprasad21@gmail.com>
2023-03-08 15:51:46 +0530

    URL generator type

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,10 @@
+<h1>About the home view</h1>
+<h1> URL Generator</h1>
+<div>
+    {{--{{URL::current()}} get current URL--}}
+    {{URL::current()}}
+    <a href="{{URL::to('about')}}">About</a>
+
+    {{--  get last URL --}}
+    <a href="{{URL::previous()}}">Last URL is :{{URL::previous()}} </a>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+
+//Route::get(path,controller file name@method name);
+
+//Route::get('/', function () {
 //    return view('user');
-    return redirect('about');
-});
+////    return redirect('about');
+//});
 
 //Route::get('/about',function (){
 //    return view('about');
 //});
 
-Route::view('/about', 'about'); // url,view name
+//Route::view('/about', 'about'); // url,view name
 
-Route::view('/contact-us', 'contact'); // url,view name
-Route::view('/user', 'user'); // url,view name
+//Route::view('/contact-us', 'contact'); // url,view name
+//Route::view('/user', 'user'); // url,view name
 //Route::get('/{name}', function ($name) {// {pass data with route}
 //    echo $name;
 //    return view('user');
 //});
 
-Route::get('/{name}', function ($name) {// {pass data with route} -> type 02
-    echo $name;
-    return view('contact', ['name' => $name]);
+//Route::get('user{name}', [\App\Http\Controllers\UserController::class, 'loadView']); // url,controller name@method name
+
+//
+//Route::get('/{name}', function ($name) {// {pass data with route} -> type 02
+//    echo $name;
+//    return view('contact', ['name' => $name]);
+//});
+
+Route::get('/home', function () {
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
