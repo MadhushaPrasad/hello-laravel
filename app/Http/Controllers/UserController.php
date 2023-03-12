c98882855bdb58f782fde7efb97f57a3109da9ab
MadhushaPrasad <madushaprasad21@gmail.com>
2023-03-12 14:57:11 +0530

    get data from database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Coupon;
+use App\Models\Users;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     //
     function saveUsers(Request $req)
     {
-        return $req->input();
+//        return $req->input();
 //        return "Return data from UserController";
+//        return DB::select("Select * from users");
+        return Coupon::all();
     }
 }
